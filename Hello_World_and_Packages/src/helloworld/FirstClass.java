dcaa2416eeacb1ae715d0ed4ec04c8ee2abe0b68
Matt Lacaire <71798926+mattl588@users.noreply.github.com>
2023-11-06 17:44:34 -0500

    First commit

---
+++ Hello_World_and_Packages/src/helloworld/FirstClass.java
@@ -0,0 +1,10 @@
+package helloworld; /*a package is not unlike just a .py script when writing python.
+It's a way to organize similar classes, and it has at most one public class.
+*/
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+}
---
+++ Hello_World_and_Packages/src/import_helloworld.java
@@ -0,0 +1,8 @@
+import helloworld.FirstClass;
+
+public class import_helloworld {
+	public static void main(String[] args) {
+		FirstClass.main(args);
+		
+	}
+}
